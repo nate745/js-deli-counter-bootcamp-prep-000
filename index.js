5a9bff5d07d0c9eb85469176d795fc916bdd1adc
Nathan Alex <nate745@gmail.com>
2019-08-12 21:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-function takeANumber(line,name){
-  line.push(name);
 
-  return (`Welcome, ${name}. You are number ${line.length} in line.`)
+
+function takeANumber(line,name){   
+  line.push(name);  /// Pushes names into array
+
+  return (`Welcome, ${name}. You are number ${line.length} in line.`) ///Returns name and number in a string
 }
 
 function nowServing(line){
-  if (line.length === 0){
+  if (line.length === 0){ /// Conditional statement that returns string
     return (`There is nobody waiting to be served!`);
   }
-  return (`Currently serving ${line.shift()}.`)
+  return (`Currently serving ${line.shift()}.`) ///Returns person in line and removes them from array
 }
 
 function currentLine(line){
-  if (line.length === 0){
+  if (line.length === 0){ /// Conditional statement that returns string
     return (`The line is currently empty.`);
   }
 
-  var numAndName = [];
+  var numAndName = [];  /// Array to hold the number and name
 
 
 /* For Loop
@@ -31,11 +33,11 @@ function currentLine(line){
 */
 
 /// While Loop
-var n = 0;
+var n = 0; /// Variable that sets initial number of people
 
-while(n < line.length){
-    numAndName.push(`${n + 1}. ${line[n]}`);
-    n++;
+while(n < line.length){ /// Conditional statement that shows how long loop will run
+    numAndName.push(`${n + 1}. ${line[n]}`); /// Pushing name and number into the array
+    n++; /// Increments position by 1
   }
-return (`The line is currently: ${numAndName.join(`, `)}`)
+return (`The line is currently: ${numAndName.join(`, `)}`) /// Returns string of people with name and number order while spacing the array out into a string
 }
